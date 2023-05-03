3ecb7001571baa483f59e9add3d8a43d75b0d3d1
neofright <68615872+neofright@users.noreply.github.com>
2023-05-03 22:44:53 +0100

    webgl

--- user.js
+++ user.js
@@ -176,8 +176,7 @@ user_pref("dom.archivereader.enabled",				false);
 // PREF: Disable webGL
 // https://en.wikipedia.org/wiki/WebGL
 // https://www.contextis.com/resources/blog/webgl-new-dimension-browser-exploitation/
-// NOTICE: Disabling WebGL breaks WebGL-based websites/applications (windy, meteoblue...)
-user_pref("webgl.disabled",					true);
+// user_pref("webgl.disabled",					true);
 // PREF: When webGL is enabled, use the minimum capability mode
 user_pref("webgl.min_capability_mode",				true);
 // PREF: When webGL is enabled, disable webGL extensions
